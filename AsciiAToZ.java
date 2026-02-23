d8466551f3b69be0158c94d2fa3ef454519083a7
nitishnr8969-maker <nitish.nr8969@gmail.com>
2026-02-23 22:14:22 +0530

    First Commit

---
+++ AsciiAToZ.java
@@ -0,0 +1,24 @@
+public class AsciiAToZ{
+    public static void main(String[] args){
+        for(char c='A'; c<='Z'; c++){
+            System.out.println(c);
+        }
+    }
+}
+
+public class AsciiAToZ{
+    public static void main(String[] args){
+        for(char c='A'; c<='Z'; c++){
+            System.out.println(c);
+        }
+    }
+}
+
+
+public class AsciiAToZ{
+    public static void main(String[] args){
+        for(char c='A'; c<='Z'; c++){
+            System.out.println(c);
+        }
+    }
+}
