abc85b9dc2049dd8ea92e9f6e14c53920f7d905a
nitishnr8969-maker <nitish.nr8969@gmail.com>
2026-02-24 22:18:26 +0530

    First Commit

---
+++ TableOf6.java
@@ -0,0 +1,24 @@
+public class tableOf6{
+    public static void main(String[] args){
+        for(int i=1; i<=10; i++){
+            System.out.println("6 x " + i + " = " + (6 * i));
+        }
+    }
+}
+
+public class tableOf6{
+    public static void main(String[] args){
+        for(int i=1; i<=10; i++){
+            System.out.println("6 x " + i + " = " + (6 * i));
+        }
+    }
+}
+
+
+public class tableOf6{
+    public static void main(String[] args){
+        for(int i=1; i<=10; i++){
+            System.out.println("6 x " + i + " = " + (6 * i));
+        }
+    }
+}
