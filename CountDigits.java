8727fc32ac7a3d8b5f34c1b94c54d6485b69a172
nitishnr8969-maker <nitish.nr8969@gmail.com>
2026-02-24 22:34:53 +0530

    First Commit

---
+++ CountDigits.java
@@ -0,0 +1,36 @@
+public class CountDigits { 
+   public static void main(String[] args) { 
+       int n = 5829; 
+       int count = 0; 
+       while(n>0) { 
+           count++; 
+           n/=10;
+       } 
+       System.out.println("Digits: " + count); 
+   } 
+}
+
+public class CountDigits { 
+   public static void main(String[] args) { 
+       int n = 5829; 
+       int count = 0; 
+       while(n>0) { 
+           count++; 
+           n/=10;
+       } 
+       System.out.println("Digits: " + count); 
+   } 
+}
+
+
+public class CountDigits { 
+   public static void main(String[] args) { 
+       int n = 5829; 
+       int count = 0; 
+       while(n>0) { 
+           count++; 
+           n/=10;
+       } 
+       System.out.println("Digits: " + count); 
+   } 
+}
