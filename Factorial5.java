08923cc6561fe5fac78f1c95b16b68fc74c3ff70
nitishnr8969-maker <nitish.nr8969@gmail.com>
2026-02-24 22:25:11 +0530

    First Commit

---
+++ Factorial5.java
@@ -0,0 +1,41 @@
+public class Factorial5{
+    public static void main(String[] args) {
+        int Factorial=1;
+        int n=5;
+        for(int i=0; i<=n; i++){
+            Factorial*=1;
+            System.err.println("Factorial: " + Factorial);
+
+        }
+
+    }
+    }
+
+    public class Factorial5{
+    public static void main(String[] args) {
+        int Factorial=1;
+        int n=5;
+        for(int i=0; i<=n; i++){
+            Factorial*=1;
+            System.err.println("Factorial: " + Factorial);
+
+        }
+
+    }
+    }
+
+
+
+public class Factorial5{
+    public static void main(String[] args) {
+        int Factorial=1;
+        int n=5;
+        for(int i=0; i<=n; i++){
+            Factorial*=1;
+            System.err.println("Factorial: " + Factorial);
+
+        }
+
+    }
+    }
+
