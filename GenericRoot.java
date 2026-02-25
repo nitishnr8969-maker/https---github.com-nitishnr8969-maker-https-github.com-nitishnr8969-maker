542476b50ca679a491a4c3bc88329fb4254bce0c
nitishnr8969-maker <nitish.nr8969@gmail.com>
2026-02-25 23:59:34 +0530

    First Commit

---
+++ GenericRoot.java
@@ -0,0 +1,41 @@
+public class GenericRoot { 
+   public static void main(String[] args) { 
+       int n = 99; 
+       for(; n > 9;) { 
+           int sum = 0; 
+           int temp = n; 
+           for(; temp > 0; temp /= 10) 
+               sum += temp % 10; 
+           n = sum; 
+       } 
+       System.out.println("Generic Root: " + n); 
+   } 
+} 
+
+public class GenericRoot { 
+   public static void main(String[] args) { 
+       int n = 99; 
+       for(; n > 9;) { 
+           int sum = 0; 
+           int temp = n; 
+           for(; temp > 0; temp /= 10) 
+               sum += temp % 10; 
+           n = sum; 
+       } 
+       System.out.println("Generic Root: " + n); 
+   } 
+} 
+
+public class GenericRoot { 
+   public static void main(String[] args) { 
+       int n = 99; 
+       for(; n > 9;) { 
+           int sum = 0; 
+           int temp = n; 
+           for(; temp > 0; temp /= 10) 
+               sum += temp % 10; 
+           n = sum; 
+       } 
+       System.out.println("Generic Root: " + n); 
+   } 
+} 
