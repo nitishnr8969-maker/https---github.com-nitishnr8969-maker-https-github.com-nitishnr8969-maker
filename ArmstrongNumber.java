0ec3090b00771b8c0ecc092da59a6a9f3cce2bce
nitishnr8969-maker <nitish.nr8969@gmail.com>
2026-02-25 23:44:44 +0530

    First Commit

---
+++ ArmstrongNumber.java
@@ -0,0 +1,47 @@
+public class ArmstrongNumber { 
+   public static void main(String[] args) { 
+       int n = 153; 
+       int temp = n; 
+       int sum = 0; 
+       for(; temp != 0; temp /= 10) { 
+           int d = temp % 10; 
+           sum += d * d * d; 
+       } 
+       if(sum == n) 
+           System.out.println("Armstrong");
+           else 
+           System.out.println("Not Armstrong"); 
+   } 
+} 
+
+public class ArmstrongNumber { 
+   public static void main(String[] args) { 
+       int n = 153; 
+       int temp = n; 
+       int sum = 0; 
+       for(; temp != 0; temp /= 10) { 
+           int d = temp % 10; 
+           sum += d * d * d; 
+       } 
+       if(sum == n) 
+           System.out.println("Armstrong");
+           else 
+           System.out.println("Not Armstrong"); 
+   } 
+} 
+
+public class ArmstrongNumber { 
+   public static void main(String[] args) { 
+       int n = 153; 
+       int temp = n; 
+       int sum = 0; 
+       for(; temp != 0; temp /= 10) { 
+           int d = temp % 10; 
+           sum += d * d * d; 
+       } 
+       if(sum == n) 
+           System.out.println("Armstrong");
+           else 
+           System.out.println("Not Armstrong"); 
+   } 
+} 
