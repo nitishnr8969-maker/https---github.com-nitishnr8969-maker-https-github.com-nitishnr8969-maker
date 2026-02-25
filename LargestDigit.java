c3ae502c1f6e1047df6954905f221fa5c1cfbe60
nitishnr8969-maker <nitish.nr8969@gmail.com>
2026-02-26 00:03:40 +0530

    First Commit

---
+++ LargestDigit.java
@@ -0,0 +1,41 @@
+public class LargestDigit { 
+   public static void main(String[] args) { 
+       int n = 48291; 
+       int max = 0; 
+       for(; n != 0; n /= 10) { 
+           int d = n % 10; 
+           if(d > max) { 
+               max = d; 
+           } 
+       } 
+       System.out.println("Max Digit: " + max); 
+   } 
+}
+
+public class LargestDigit { 
+   public static void main(String[] args) { 
+       int n = 48291; 
+       int max = 0; 
+       for(; n != 0; n /= 10) { 
+           int d = n % 10; 
+           if(d > max) { 
+               max = d; 
+           } 
+       } 
+       System.out.println("Max Digit: " + max); 
+   } 
+}
+
+public class LargestDigit { 
+   public static void main(String[] args) { 
+       int n = 48291; 
+       int max = 0; 
+       for(; n != 0; n /= 10) { 
+           int d = n % 10; 
+           if(d > max) { 
+               max = d; 
+           } 
+       } 
+       System.out.println("Max Digit: " + max); 
+   } 
+}
