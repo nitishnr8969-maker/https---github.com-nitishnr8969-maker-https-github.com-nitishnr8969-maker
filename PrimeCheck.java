c18fc784271cc8e5358e50cbd94f98815cb62b81
nitishnr8969-maker <nitish.nr8969@gmail.com>
2026-02-25 19:16:31 +0530

    updated code

--- PrimeCheck.java
+++ PrimeCheck.java
@@ -18,46 +18,5 @@ public class PrimeCheck{
 }
 
 
-public class PrimeCheck{
-    public static void main(String[] args) {
-    int n=29;
-    boolean isPrime=true;
-    for(int i=2; i<=n/2; i++){
-        if(n%i==0){
-            isPrime=false;
-            break;
-        }
-    }
-    if(isPrime && n>1){
-        System.out.println("prime");
-    }
-    else{
-        System.out.println("not prime");
-    }
-    }
-}
-
-
-
-public class PrimeCheck{
-    public static void main(String[] args) {
-    int n=29;
-    boolean isPrime=true;
-    for(int i=2; i<=n/2; i++){
-        if(n%i==0){
-            isPrime=false;
-            break;
-        }
-    }
-    if(isPrime && n>1){
-        System.out.println("prime");
-    }
-    else{
-        System.out.println("not prime");
-    }
-    }
-}
-
-
 
 
