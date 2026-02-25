22bb16aa47083e7bd5cadd3cc178c8365bcb3b43
nitishnr8969-maker <nitish.nr8969@gmail.com>
2026-02-25 23:40:24 +0530

    First Commit

---
+++ class SumAP.java
@@ -0,0 +1,39 @@
+public class SumAP { 
+   public static void main(String[] args) { 
+       int a = 1, d = 3, n = 5; 
+       int sum = 0; 
+       int current = a; 
+       for(int i = 1; i <= n; i++) { 
+           sum += current; 
+           current += d; 
+       } 
+       System.out.println("Sum: " + sum); 
+   } 
+}
+
+public class SumAP { 
+   public static void main(String[] args) { 
+       int a = 1, d = 3, n = 5; 
+       int sum = 0; 
+       int current = a; 
+       for(int i = 1; i <= n; i++) { 
+           sum += current; 
+           current += d; 
+       } 
+       System.out.println("Sum: " + sum); 
+   } 
+}
+
+public class SumAP { 
+   public static void main(String[] args) { 
+       int a = 1, d = 3, n = 5; 
+       int sum = 0; 
+       int current = a; 
+       for(int i = 1; i <= n; i++) { 
+           sum += current; 
+           current += d; 
+       } 
+       System.out.println("Sum: " + sum); 
+   } 
+}
+
