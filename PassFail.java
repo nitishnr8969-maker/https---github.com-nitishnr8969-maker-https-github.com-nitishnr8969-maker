551641d741263746c0483c7741e00b9844388509
nitishnr8969-maker <nitish.nr8969@gmail.com>
2026-02-27 23:37:14 +0530

    First Commit

---
+++ PassFail.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class PassFail{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int marks=sc.nextInt();
+        if(marks>=40){
+            System.out.print("Pass");
+        }
+        else{
+            System.out.print("Fail");
+        }
+        }
+    }
+
+
+import java.util.Scanner;
+public class PassFail{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int marks=sc.nextInt();
+        if(marks>=40){
+            System.out.print("Pass");
+        }
+        else{
+            System.out.print("Fail");
+        }
+        }
+    }
+
+
+import java.util.Scanner;
+public class PassFail{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int marks=sc.nextInt();
+        if(marks>=40){
+            System.out.print("Pass");
+        }
+        else{
+            System.out.print("Fail");
+        }
+        }
+    }
