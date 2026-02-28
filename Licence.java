71ae14bff81c613aee5239b377302f6717fdf314
nitishnr8969-maker <nitish.nr8969@gmail.com>
2026-02-28 22:26:48 +0530

    First Commit

---
+++ Licence.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Licence{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter age: ");
+int age = sc.nextInt();
+if(age >= 18)
+System.out.println("Can Apply for License");
+else
+System.out.println("Not Eligible");
+}
+}
+
+import java.util.Scanner;
+public class Licence{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter age: ");
+int age = sc.nextInt();
+if(age >= 18)
+System.out.println("Can Apply for License");
+else
+System.out.println("Not Eligible");
+}
+}
+
+import java.util.Scanner;
+public class Licence{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter age: ");
+int age = sc.nextInt();
+if(age >= 18)
+System.out.println("Can Apply for License");
+else
+System.out.println("Not Eligible");
+}
+}
