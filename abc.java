518339aa14f36a2ab62f511912ba4a7b34c5fa9f
nitishnr8969-maker <nitish.nr8969@gmail.com>
2026-02-28 22:17:27 +0530

    First Commit

---
+++ abc.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class abc{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter BMI value: ");
+double bmi = sc.nextDouble();
+if(bmi < 18.5)
+System.out.println("Underweight");
+else if(bmi < 25)
+System.out.println("Normal");
+Soumik Karmakar - Object Oriented Programming with Java
+else
+System.out.println("Overweight");
+}
+}
+
+import java.util.Scanner;
+public class abc{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter BMI value: ");
+double bmi = sc.nextDouble();
+if(bmi < 18.5)
+System.out.println("Underweight");
+else if(bmi < 25)
+System.out.println("Normal");
+Soumik Karmakar - Object Oriented Programming with Java
+else
+System.out.println("Overweight");
+}
+}
+
+import java.util.Scanner;
+public class abc{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter BMI value: ");
+double bmi = sc.nextDouble();
+if(bmi < 18.5)
+System.out.println("Underweight");
+else if(bmi < 25)
+System.out.println("Normal");
+Soumik Karmakar - Object Oriented Programming with Java
+else
+System.out.println("Overweight");
+}
+}
