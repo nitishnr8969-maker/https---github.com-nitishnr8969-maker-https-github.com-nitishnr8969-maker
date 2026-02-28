ad9d924fb9bd447cbd3a9115d3167d29cca802f9
nitishnr8969-maker <nitish.nr8969@gmail.com>
2026-02-28 17:47:31 +0530

    First Commit

---
+++ ProfitLoss.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class ProfitLoss{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Cost Price :");
+        int costPrice=sc.nextInt();
+        System.out.print("Enter Selling Price :");
+        int sellingPrice=sc.nextInt();
+        if(sellingPrice>costPrice){
+            System.out.print("Profit");
+        }
+        else{
+            System.out.print("Loss");
+        }
+        }
+    }
+
+    import java.util.Scanner;
+public class ProfitLoss{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Cost Price :");
+        int costPrice=sc.nextInt();
+        System.out.print("Enter Selling Price :");
+        int sellingPrice=sc.nextInt();
+        if(sellingPrice>costPrice){
+            System.out.print("Profit");
+        }
+        else{
+            System.out.print("Loss");
+        }
+        }
+    }
+
+    import java.util.Scanner;
+public class ProfitLoss{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Cost Price :");
+        int costPrice=sc.nextInt();
+        System.out.print("Enter Selling Price :");
+        int sellingPrice=sc.nextInt();
+        if(sellingPrice>costPrice){
+            System.out.print("Profit");
+        }
+        else{
+            System.out.print("Loss");
+        }
+        }
+    }
