bd230d14d55aede3a9ea63d31a78e6134ac613f8
nitishnr8969-maker <nitish.nr8969@gmail.com>
2026-02-28 21:55:56 +0530

    First Commit

---
+++ ElectricityBill.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class ElectricityBill{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter units consumed:");
+        int units=sc.nextInt();
+        int bill;
+        if(units<=100){
+            bill=units*5;
+            System.out.print("Total Bill: " + bill);
+        }
+        else 
+        bill=100*5+(units-100)*10;
+        System.out.print("Total Bill: " + bill);
+    }
+}
+
+import java.util.Scanner;
+public class ElectricityBill{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter units consumed:");
+        int units=sc.nextInt();
+        int bill;
+        if(units<=100){
+            bill=units*5;
+            System.out.print("Total Bill: " + bill);
+        }
+        else 
+        bill=100*5+(units-100)*10;
+        System.out.print("Total Bill: " + bill);
+    }
+}
+
+import java.util.Scanner;
+public class ElectricityBill{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter units consumed:");
+        int units=sc.nextInt();
+        int bill;
+        if(units<=100){
+            bill=units*5;
+            System.out.print("Total Bill: " + bill);
+        }
+        else 
+        bill=100*5+(units-100)*10;
+        System.out.print("Total Bill: " + bill);
+    }
+}
