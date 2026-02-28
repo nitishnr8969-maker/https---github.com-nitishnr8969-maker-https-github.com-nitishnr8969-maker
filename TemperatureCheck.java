fa962c0080a5a1b18e487f671af6d033eb9f894c
nitishnr8969-maker <nitish.nr8969@gmail.com>
2026-02-28 17:06:51 +0530

    First Commit

---
+++ TemperatureCheck.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class TemperatureCheck{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int temp=sc.nextInt();
+        if(temp>35){
+            System.out.print("Hot");
+        }
+        else{
+            System.out.print("Cold");
+        }
+    }
+}
+
+import java.util.Scanner;
+public class TemperatureCheck{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int temp=sc.nextInt();
+        if(temp>35){
+            System.out.print("Hot");
+        }
+        else{
+            System.out.print("Cold");
+        }
+    }
+}
+
+import java.util.Scanner;
+public class TemperatureCheck{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int temp=sc.nextInt();
+        if(temp>35){
+            System.out.print("Hot");
+        }
+        else{
+            System.out.print("Cold");
+        }
+    }
+}
