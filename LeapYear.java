7cc182d67539b1e0fcbae8a85c37d30cb8f07f42
nitishnr8969-maker <nitish.nr8969@gmail.com>
2026-02-28 17:54:39 +0530

    First  Commit

---
+++ LeapYear.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class LeapYear{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a Year:");
+        int Year=sc.nextInt();
+        if((Year%4==0 && Year%100!=0) ||(Year%400==0)){
+            System.out.print("Leap Year");
+        }
+        else{
+            System.out.print("Not a Leap Year");
+        }
+    }
+}
+
+import java.util.Scanner;
+public class LeapYear{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a Year:");
+        int Year=sc.nextInt();
+        if((Year%4==0 && Year%100!=0) ||(Year%400==0)){
+            System.out.print("Leap Year");
+        }
+        else{
+            System.out.print("Not a Leap Year");
+        }
+    }
+}
+
+import java.util.Scanner;
+public class LeapYear{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a Year:");
+        int Year=sc.nextInt();
+        if((Year%4==0 && Year%100!=0) ||(Year%400==0)){
+            System.out.print("Leap Year");
+        }
+        else{
+            System.out.print("Not a Leap Year");
+        }
+    }
+}
