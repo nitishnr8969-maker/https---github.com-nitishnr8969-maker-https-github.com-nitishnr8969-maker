56e53796c46d4604e5ca303831227e7c77eb9ae2
nitishnr8969-maker <nitish.nr8969@gmail.com>
2026-02-28 16:58:26 +0530

    First Commit

---
+++ DivisibleBy5.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class DivisibleBy5{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%5==0){
+            System.out.print("Divisible by 5");
+        }
+        else{
+            System.out.print("Not Divisible by 5");
+        }
+    }
+}
+
+import java.util.Scanner;
+public class DivisibleBy5{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%5==0){
+            System.out.print("Divisible by 5");
+        }
+        else{
+            System.out.print("Not Divisible by 5");
+        }
+    }
+}
+
+import java.util.Scanner;
+public class DivisibleBy5{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%5==0){
+            System.out.print("Divisible by 5");
+        }
+        else{
+            System.out.print("Not Divisible by 5");
+        }
+    }
+}
